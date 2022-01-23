c5ed8fe6ab052af95885fc2d92678aebe7d6bc55
Kaan Genc <genc.5@osu.edu>
2022-01-23 03:22:41 -0500

    fix type issues with tasks

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-use std::collections::HashMap;
 use crate::config::Config;
-use reqwest::{header, Client, ClientBuilder};
-use std::error::Error;
-use structopt::StructOpt;
-use tokio;
+use anyhow;
 use futures;
+use reqwest::{header, Client, ClientBuilder, StatusCode};
+use std::collections::HashMap;
+use structopt::StructOpt;
+use tokio::{self, task::JoinHandle};
 mod config;
 mod opts;
 
@@ -13,10 +13,13 @@ fn gandi_api_get(fqdn: &str) -> String {
 }
 
 fn gandi_api_url(fqdn: &str, rrset_name: &str, rrset_type: &str) -> String {
-    return format!(" https://api.gandi.net/v5/livedns/domains/{}/records/{}/{}", fqdn, rrset_name, rrset_type);
+    return format!(
+        " https://api.gandi.net/v5/livedns/domains/{}/records/{}/{}",
+        fqdn, rrset_name, rrset_type
+    );
 }
 
-fn api_client(api_key: &str) -> Result<Client, Box<dyn Error>> {
+fn api_client(api_key: &str) -> anyhow::Result<Client> {
     let client_builder = ClientBuilder::new();
 
     let key = format!("Apikey {}", api_key);
@@ -31,7 +34,7 @@ fn api_client(api_key: &str) -> Result<Client, Box<dyn Error>> {
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> anyhow::Result<()> {
     let opts = opts::Opts::from_args();
     let conf_path = config::config_path(&opts);
     println!("Loading config from {:#?}", conf_path);
@@ -43,26 +46,40 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let ipv4 = String::from("173.89.215.91");
     let ipv6 = String::from("2603:6011:be07:302:79f4:50dd:6abe:be38");
 
-    let mut results = Vec::new();
+    let mut results: Vec<JoinHandle<(StatusCode, String)>> = Vec::new();
 
     for entry in &conf.entry {
         for entry_type in Config::types(entry) {
             let fqdn = Config::fqdn(&entry, &conf);
             let url = gandi_api_url(fqdn, entry.name.as_str(), entry_type);
-            let ip = if entry_type.eq("A") { ipv4.as_str() } else { ipv6.as_str() };
+            let ip = if entry_type.eq("A") {
+                ipv4.as_str()
+            } else {
+                ipv6.as_str()
+            };
             let mut map = HashMap::new();
             map.insert("rrset_values", ip);
             let req = client.put(url).json(&map);
             let task = tokio::task::spawn(async move {
-                let response = req.send().await?;
-                return (response.status(), response.text().await?);
+                match req.send().await {
+                    Ok(response) => (
+                        response.status(),
+                        response
+                            .text()
+                            .await
+                            .unwrap_or_else(|error| error.to_string()),
+                    ),
+                    Err(error) => (
+                        StatusCode::IM_A_TEAPOT, error.to_string()
+                    ),
+                }
             });
             results.push(task);
         }
     }
-    
+
     let results = futures::future::try_join_all(results).await?;
-    
+
     for (status, body) in results {
         println!("{} - {}", status, body);
     }
