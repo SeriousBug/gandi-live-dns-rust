74f131f4be12860e82852b58926647b858df1821
Kaan Genc <kaan@bgenc.net>
2022-04-03 23:54:26 -0400

    add the config option for the flag

--- src/opts.rs
+++ src/opts.rs
@@ -1,4 +1,46 @@
-use clap::{Parser, ArgEnum};
+use clap::{ArgEnum, Parser};
+use std::{fmt::Display, str::FromStr};
+use thiserror::Error;
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ArgEnum)]
+pub enum SilenceLevel {
+    All,
+    Domains,
+}
+
+impl Display for SilenceLevel {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            SilenceLevel::All => "all",
+            SilenceLevel::Domains => "domains",
+        })
+    }
+}
+
+#[derive(Debug, Clone, Error)]
+pub struct SilenceLevelError {
+    pub message: String,
+}
+
+impl FromStr for SilenceLevel {
+    type Err = SilenceLevelError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s.to_lowercase().as_str() {
+            "all" => Ok(SilenceLevel::All),
+            "domains" => Ok(SilenceLevel::Domains),
+            option => Err(SilenceLevelError {
+                message: format!("Bad option {}, should be `{}` or `{}`", option, SilenceLevel::All, SilenceLevel::Domains),
+            }),
+        }
+    }
+}
+
+impl Display for SilenceLevelError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(self.message.as_str())
+    }
+}
 
 /// A tool to automatically update DNS entries on Gandi, using it as a dynamic DNS system.
 #[derive(Parser, Debug)]
@@ -8,4 +50,6 @@ pub struct Opts {
     #[clap(long)]
     pub config: Option<String>,
 
+    #[clap(long)]
+    pub silent: Option<SilenceLevel>,
 }
