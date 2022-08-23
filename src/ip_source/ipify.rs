0e643bc9c2372da6a342047adc142e952eebf289
Kaan Barmore-Genc <kaan@bgenc.net>
2022-08-22 22:15:28 -0400

    fix bad format

--- src/ip_source/ipify.rs
+++ src/ip_source/ipify.rs
@@ -6,17 +6,17 @@ use super::ip_source::IPSource;
 pub(crate) struct IPSourceIpify {}
 
 async fn get_ip(api_url: &str) -> anyhow::Result<String> {
-  let response = reqwest::get(api_url).await?;
-  let text = response.text().await?;
-  Ok(text)
+    let response = reqwest::get(api_url).await?;
+    let text = response.text().await?;
+    Ok(text)
 }
 
 #[async_trait]
 impl IPSource for IPSourceIpify {
-  async fn get_ipv4() -> anyhow::Result<String> {
-    get_ip("https://api.ipify.org").await
-  }
-  async fn get_ipv6() -> anyhow::Result<String> {
-    get_ip("https://api6.ipify.org").await
-  }
+    async fn get_ipv4() -> anyhow::Result<String> {
+        get_ip("https://api.ipify.org").await
+    }
+    async fn get_ipv6() -> anyhow::Result<String> {
+        get_ip("https://api6.ipify.org").await
+    }
 }
