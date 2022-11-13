5755aedc2fb6a25bfdf0409a18d17cf22b240edb
Kaan Barmore-Genç <kaan@bgenc.net>
2022-11-13 15:36:21 -0500

    Add CLI option to skip ipv4 or ipv6 (#58)
    closes #7

--- src/opts.rs
+++ src/opts.rs
@@ -1,10 +1,20 @@
 use clap::Parser;
 
 /// A tool to automatically update DNS entries on Gandi, using it as a dynamic DNS system.
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Default)]
 #[clap(author, version, about, long_about = None, name = "gandi-live-dns")]
 pub struct Opts {
     /// The path to the configuration file.
     #[clap(long)]
     pub config: Option<String>,
+    /// Skip IPv4 updates.
+    ///
+    /// If enabled, any IPv4 (A) records in the configuration file are ignored.
+    #[clap(action, long)]
+    pub skip_ipv4: bool,
+    /// Skip IPv4 updates.
+    ///
+    /// If enabled, any IPv6 (AAAA) records in the configuration file are ignored.
+    #[clap(action, long)]
+    pub skip_ipv6: bool,
 }
