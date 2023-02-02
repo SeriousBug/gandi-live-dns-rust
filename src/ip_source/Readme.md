08b5505d25ee60b632968a7e225eb3af87b4ca82
Kaan Barmore-Genc <kaan@bgenc.net>
2023-02-01 23:06:58 -0500

    Add new IP source "seeip"

--- src/ip_source/Readme.md
+++ src/ip_source/Readme.md
@@ -2,5 +2,5 @@ The IP sources. These are APIs that we can query to get the IP address of the
 current service.
 
 The tests under this directory are all marked to be skipped, the tests hit the
-actual APIs and can be flakey in CI. Make sure to run the tests manually if you
+actual APIs and can be flaky in CI. Make sure to run the tests manually if you
 have to modify the code.
---
+++ src/ip_source/seeip.rs
@@ -0,0 +1,53 @@
+use async_trait::async_trait;
+
+use super::ip_source::IPSource;
+
+pub(crate) struct IPSourceSeeIP;
+
+async fn get_ip(api_url: &str) -> anyhow::Result<String> {
+    let response = reqwest::get(api_url).await?;
+    let text = response.text().await?;
+    Ok(text)
+}
+
+#[async_trait]
+impl IPSource for IPSourceSeeIP {
+    async fn get_ipv4(&self) -> anyhow::Result<String> {
+        get_ip("https://ip4.seeip.org").await
+    }
+    async fn get_ipv6(&self) -> anyhow::Result<String> {
+        get_ip("https://ip6.seeip.org").await
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use regex::Regex;
+
+    use super::IPSource;
+    use super::IPSourceSeeIP;
+
+    #[tokio::test]
+    #[ignore]
+    async fn ipv4_test() {
+        let ipv4 = IPSourceSeeIP
+            .get_ipv4()
+            .await
+            .expect("Failed to get the IP address");
+        assert!(Regex::new(r"^\d+[.]\d+[.]\d+[.]\d+$")
+            .unwrap()
+            .is_match(ipv4.as_str()))
+    }
+
+    #[tokio::test]
+    #[ignore]
+    async fn ipv6_test() {
+        let ipv6 = IPSourceSeeIP
+            .get_ipv6()
+            .await
+            .expect("Failed to get the IP address");
+        assert!(Regex::new(r"^([0-9a-fA-F]*:){7}[0-9a-fA-F]*$")
+            .unwrap()
+            .is_match(ipv6.as_str()))
+    }
+}
